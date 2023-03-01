52390efe9a00230bdcee67b22449a7eef8ad8d0d
CindyMisoi <crmisoi2k@gmail.com>
2023-03-01 13:32:56 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
-
+  # Add this line to set the Content-Type header for all responses
+ set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+     # send them back as a JSON array
+    bakeries.to_json
+  end
 
+  get '/bakeries/:id' do
+   # look up the bakery in the database using its ID
+   bakery = Bakery.find(params[:id])
+   bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_good = BakedGood.all.order(price: :desc)
+    baked_good.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked_good =  BakedGood.all.order(price: :asc).last
+    baked_good.to_json
+  end
 end
